dad549a544f11f6f59a7de8d1fb2566e2e34fb5a
Chris Boyce <christopher.d.boyce@gmail.com>
2016-03-09 14:47:40 -0800

    First pass at model in FreeCAD

---
+++ copy_to_dropbox.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -a . /media/cboyce/Dropbox\ Drive/Dropbox/Projects/Metronome/V3/ 
+rm -rf /media/cboyce/Dropbox\ Drive/Dropbox/Projects/Metronome/V3/.git/ 
+rm  /media/cboyce/Dropbox\ Drive/Dropbox/Projects/Metronome/V3/copy_to_dropbox.sh
+
